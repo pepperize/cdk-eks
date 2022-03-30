60e66e19ba272ce9708e88b663fafbb41bafb004
Patrick Florek <patrick.florek@gmail.com>
2022-03-30 11:47:28 +0200

    chore: separate stacks

---
+++ cluster.ts
@@ -0,0 +1,65 @@
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as eks from "aws-cdk-lib/aws-eks";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Karpenter } from "cdk-karpenter";
+import { Construct } from "constructs";
+import { AwsLoadBalancerController } from "./aws-load-balancer-controller";
+import { ExternalDns } from "./external-dns";
+
+export interface BaseClusterProps {
+  readonly clusterName?: string;
+}
+
+export interface ClusterProps extends BaseClusterProps {
+  readonly hostedZoneId: string;
+  readonly vpc: ec2.IVpc;
+  readonly mainRoles: iam.IRole[];
+}
+
+export class Cluster extends Construct {
+  public readonly mainRole: iam.IRole;
+  public readonly cluster: eks.ICluster;
+  public readonly autoscaling: Karpenter;
+  public readonly loadBalancer: AwsLoadBalancerController;
+  public readonly externalDns: ExternalDns;
+
+  public constructor(scope: Construct, id: string, props: ClusterProps) {
+    super(scope, id);
+
+    this.mainRole = new iam.Role(this, "MainRole", {
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
+
+    this.cluster = new eks.Cluster(this, "Cluster", {
+      vpc: props.vpc,
+      mastersRole: this.mainRole,
+      clusterName: props.clusterName ?? "Cluster",
+      version: eks.KubernetesVersion.V1_21,
+      defaultCapacityInstance: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MEDIUM),
+    });
+
+    const awsAuth = (this.cluster as eks.Cluster).awsAuth;
+    for (const role of props.mainRoles) {
+      awsAuth.addMastersRole(role);
+    }
+
+    // auto scale
+    // https://artifacthub.io/packages/helm/karpenter/karpenter
+    this.autoscaling = new Karpenter(this, "Karpenter", {
+      cluster: this.cluster as eks.Cluster,
+      vpc: props.vpc,
+    });
+
+    // load balancing
+    this.loadBalancer = new AwsLoadBalancerController(this, "AwsLoadBalancerController", {
+      cluster: this.cluster,
+      vpcId: props.vpc.vpcId,
+    });
+
+    // domain name
+    this.externalDns = new ExternalDns(this, "ExternalDns", {
+      cluster: this.cluster,
+      hostedZoneId: props.hostedZoneId,
+    });
+  }
+}
---
+++ external-dns.ts
@@ -0,0 +1,71 @@
+import * as eks from "aws-cdk-lib/aws-eks";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+export interface ExternalDnsProps {
+  readonly cluster: eks.ICluster;
+  readonly hostedZoneId: string;
+  /**
+   * @default dns
+   */
+  readonly namespace?: string;
+}
+
+export class ExternalDns extends Construct {
+  readonly cluster: eks.ICluster;
+
+  public constructor(scope: Construct, id: string, props: ExternalDnsProps) {
+    super(scope, id);
+
+    this.cluster = props.cluster;
+    const namespace = props.namespace ?? "dns";
+
+    const externalsDnsNamespaceManifest = new eks.KubernetesManifest(this, "DnsNamespace", {
+      cluster: this.cluster,
+      manifest: [
+        {
+          apiVersion: "v1",
+          kind: "Namespace",
+          metadata: {
+            name: namespace,
+          },
+        },
+      ],
+    });
+
+    // https://artifacthub.io/packages/helm/bitnami/external-dns
+    const externalDnsServiceAccount = this.cluster.addServiceAccount("ExternalDnsServiceAccount", {
+      name: "external-dns",
+      namespace: namespace,
+    });
+    externalDnsServiceAccount.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ["route53:ChangeResourceRecordSets", "route53:ListResourceRecordSets"],
+        resources: [`arn:aws:route53:::hostedzone/${props.hostedZoneId}`],
+      })
+    );
+    externalDnsServiceAccount.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        actions: ["route53:ListHostedZones"],
+        resources: ["*"],
+      })
+    );
+    externalDnsServiceAccount.node.addDependency(externalsDnsNamespaceManifest);
+
+    const externalDnsChart = this.cluster.addHelmChart("ExternalDns", {
+      repository: "https://charts.bitnami.com/bitnami",
+      chart: "external-dns",
+      release: "external-dns",
+      namespace: namespace,
+      version: "6.1.8",
+      values: {
+        zoneIdFilters: [props.hostedZoneId],
+        serviceAccount: {
+          create: false,
+          name: externalDnsServiceAccount.serviceAccountName,
+        },
+      },
+    });
+    externalDnsChart.node.addDependency(externalDnsServiceAccount, externalsDnsNamespaceManifest);
+  }
+}

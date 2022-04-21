064814b3a98820fa43349ee11c148371c3939c2d
Patrick Florek <patrick.florek@gmail.com>
2022-04-21 20:44:31 +0200

    chore(deps): add cdk-nag
    Co-authored-by: Andreas Forster <andreas.forster@pepperize.com>

--- .projenrc.js
+++ .projenrc.js
@@ -4,12 +4,11 @@ const project = new AwsCdkConstructLibrary({
   authorAddress: "andreas.forster@pepperize.com",
   cdkVersion: "2.20.0",
   defaultReleaseBranch: "main",
-  devDeps: ["@pepperize/projen-awscdk-construct"],
   name: "cdk-eks",
   repositoryUrl: "git@github.com:pepperize/cdk-eks.git",
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   packageName: "@pepperize/cdk-eks",
+
+  devDeps: ["@pepperize/projen-awscdk-construct", "cdk-nag"],
 });
 project.synth();

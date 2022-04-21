c7ed4036ae37ad1a18a206cba3357176175c0774
Patrick Florek <patrick.florek@gmail.com>
2022-04-21 21:43:46 +0200

    Revert "feat: release pypi .net"

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,5 @@
 const { AwsCdkConstructLibrary } = require("@pepperize/projen-awscdk-construct");
-const { cdk, javascript } = require("projen");
 const project = new AwsCdkConstructLibrary({
-  stability: cdk.Stability.EXPERIMENTAL,
-
   author: "Andreas Forster",
   authorAddress: "andreas.forster@pepperize.com",
   cdkVersion: "2.20.0",
@@ -12,19 +9,8 @@ const project = new AwsCdkConstructLibrary({
 
   packageName: "@pepperize/cdk-eks",
 
-  bundledDeps: ["cdk-karpenter"],
+  peerDeps: ["cdk-karpenter"],
   devDeps: ["@pepperize/projen-awscdk-construct", "cdk-nag"],
-
-  releaseToNpm: true,
-  npmAccess: javascript.NpmAccess.PUBLIC,
-  publishToNuget: {
-    dotNetNamespace: "Pepperize.CDK",
-    packageId: "Pepperize.CDK.Eks",
-  },
-  publishToPypi: {
-    distName: "pepperize.cdk-eks",
-    module: "pepperize_cdk_eks",
-  },
 
   gitpod: true,
 });
